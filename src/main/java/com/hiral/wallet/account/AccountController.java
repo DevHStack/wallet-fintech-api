1fa3898f9a6a307ee2afa3dde0166a05742674c0
DevHStack <hiralshivade@gmail.com>
2026-03-02 13:14:25 +0100

    wip: debug controller 404 issue

--- src/main/java/com/hiral/wallet/account/AccountController.java
+++ src/main/java/com/hiral/wallet/account/AccountController.java
@@ -1,9 +1,9 @@
 package com.hiral.wallet.account;
 
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import com.hiral.wallet.account.DepositRequest;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -12,9 +12,12 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    @PostMapping
-    public ResponseEntity<Account> createAccount(@RequestBody CreateAccountRequest request) {
-        Account account = accountService.createAccount(request.getOwnerName());
-        return ResponseEntity.status(201).body(account);
-    }
+    @PostMapping("/{id}/deposit")
+public ResponseEntity<Account> deposit(
+        @PathVariable Long id,
+        @RequestBody DepositRequest request) {
+
+    Account updatedAccount = accountService.deposit(id, request.getAmount());
+    return ResponseEntity.ok(updatedAccount);
+}
 }
